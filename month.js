39e70efc4362dd2c5e8ecd631afd6976a04e4371
Phi Hung <phihunds1@gmail.com>
2020-10-23 12:27:03 +0700

    tao lap

---
+++ month.js
@@ -0,0 +1,10 @@
+function tinhSoNgay() {
+    let month = Number(document.getElementById("month").value)
+    switch (month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+    }
+}
